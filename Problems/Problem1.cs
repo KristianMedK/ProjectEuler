99f261d9f186155363939ba1120747d184e507c3
Kristian Overgaard <kov@flexfone.dk>
2023-01-06 07:28:40 +0100

    Problem 1

---
+++ Problems/Problem1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+
+    //Sum of FizzBuzz numbers
+    public static class Problem1
+    {
+        public static void Solve()
+        {
+            var max = 1000;
+            var sum = 0;
+            for(int i =0; i< max; i++)
+            {
+                if(i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"Problem 1 answer: {sum}");
+        }
+    }
+}
---
+++ Problems/Problem2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+
+    //Even Fibonacci numbers
+    public static class Problem2
+    {
+        public static void Solve()
+        {
+
+        }
+    }
+}
